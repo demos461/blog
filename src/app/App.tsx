a19f877fc63550c4db4faeadd0dd6e508af88d04
demos461 <iamdemos461@gmail.com>
2023-09-07 15:27:30 +0300

    configure project structure to Feature-Sliced Design, configure absolute import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
-import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -18,8 +18,8 @@ const App = () => {
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
 
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
