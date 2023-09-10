48338b51a44af5ae7cfb400d0a2566c6d6eabcb5
demos461 <iamdemos461@gmail.com>
2023-09-10 14:35:00 +0300

    create sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -5,11 +5,14 @@ import {routeConfig} from "shared/config/routeConfig/routeConfig";
 const AppRouter = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({element, path}) => (
-                   <Route key={path} path={path} element={element} />
-                ))}
-            </Routes>
+            <div className='page-wrapper'>
+                <Routes>
+                    {Object.values(routeConfig).map(({element, path}) => (
+                        <Route key={path} path={path} element={element}/>
+
+                    ))}
+                </Routes>
+            </div>
         </Suspense>
     );
 };
