a32ce6e881d4ad898d4d03bf5eb5440dae31147e
demos461 <iamdemos461@gmail.com>
2023-09-15 16:17:47 +0300

    add storybook, create webpack config for storybook, create style, theme and router decorators, create stories

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { BuildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     // Если не используем typescript - нужен babel-loader
@@ -24,24 +24,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoader = BuildCssLoader(isDev);
 
     return [
         svgLoader,
