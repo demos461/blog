d295e1c09ed574418dd5b6cc89186e4cfe3f5608
demos461 <iamdemos461@gmail.com>
2023-09-16 12:47:08 +0300

    fix toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,36 +1,38 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = ({ className }: SidebarProps) =>
-// const [collapsed, setCollapsed] = useState(false);
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
 
-// const onToggle = () => {
-//     setCollapsed((prev) => !prev);
-// };
-
-    // eslint-disable-next-line implicit-arrow-linebreak
-    (
+    return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, {}, [className])}
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            {/* <Button */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    type="button" */}
-            {/*    onClick={onToggle} */}
-            {/* > */}
-            {/*    toggle */}
-            {/* </Button> */}
+            <Button
+                data-testid="sidebar-toggle"
+                type="button"
+                onClick={onToggle}
+            >
+                {t('Переключить')}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
+};
