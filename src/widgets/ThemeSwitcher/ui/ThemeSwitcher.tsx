a7c853b63b747255ad66308a6716b59ae2f569b8
demos461 <iamdemos461@gmail.com>
2023-10-04 19:53:02 +0300

    add new theme

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import TealIcon from 'shared/assets/icons/theme-teal.svg';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { memo } from 'react';
 
@@ -12,13 +13,19 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
+    let currentTheme;
+
+    if (theme === Theme.LIGHT) currentTheme = <LightIcon />;
+    if (theme === Theme.DARK) currentTheme = <DarkIcon />;
+    if (theme === Theme.TEAL) currentTheme = <TealIcon />;
+
     return (
         <Button
             theme={ButtonTheme.CLEAR}
             className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
-            {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+            {currentTheme}
         </Button>
     );
 });
