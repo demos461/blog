8fe26414c47662a51dd8b208dd7ce2bbea611551
demos461 <iamdemos461@gmail.com>
2023-10-06 15:34:59 +0300

    update app router, create RequireAuth, add _initiated to userSlice

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,28 +1,27 @@
-import React, { memo, Suspense, useMemo } from 'react';
+import React, { memo, Suspense, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
-import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { RequireAuth } from './RequireAuth';
 
 const AppRouter = () => {
-    const isAuth = useSelector(getUserAuthData);
-
-    const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
-        if (route.authOnly && !isAuth) {
-            return false;
-        }
-
-        return true;
-    }), [isAuth]);
+    const renderWithWrapper = useCallback((route: AppRoutesProps) => (
+        <Route
+            key={route.path}
+            path={route.path}
+            element={
+                route.authOnly
+                    ? <RequireAuth><div>{route.element}</div></RequireAuth>
+                    : route.element
+            }
+        />
+    ), []);
 
     return (
         <Suspense fallback={<PageLoader />}>
             <div className="page-wrapper">
                 <Routes>
-                    {routes.map(({ element, path }) => (
-                        <Route key={path} path={path} element={element} />
-                    ))}
+                    {Object.values(routeConfig).map(renderWithWrapper)}
                 </Routes>
             </div>
         </Suspense>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
+export { getUserInitiated } from './model/selectors/getUserInited/getUserInitiated';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
 export { userReducer, userActions } from './model/slice/userSlice';
 export { User, UserSchema } from './types/user';
