6387f259549af3b3d6a859aeac43b9c66725c38d
demos461 <iamdemos461@gmail.com>
2023-11-18 00:20:51 +0300

    upgrade webpack config, add babel cache

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -6,6 +6,7 @@ interface BuildBabelLoaderProps extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
+    const isProd = !isDev;
     return {
         test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
         exclude: /node_modules/,
@@ -13,6 +14,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
             {
                 loader: 'babel-loader',
                 options: {
+                    cacheDirectory: true,
                     plugins: [
                         [
                             '@babel/plugin-transform-typescript',
@@ -21,7 +23,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
                             },
                         ],
                         '@babel/plugin-transform-runtime',
-                        isTsx && [
+                        isTsx && isProd && [
                             babelRemovePropsPlugin,
                             {
                                 props: ['data-testid'],
