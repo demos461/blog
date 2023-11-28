3316bc092ed54520573163289396d96ad5b6fd89
demos461 <iamdemos461@gmail.com>
2023-11-28 22:08:16 +0300

    add stories

--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -1,5 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import { Loader } from '../Loader';
+import { Text, TextTheme } from '../Text';
+
 import { AppImage } from './AppImage';
 
 const meta:Meta<typeof AppImage> = {
@@ -11,5 +14,21 @@ export default meta;
 type Story = StoryObj<typeof AppImage>
 
 export const Primary: Story = {
-    args: {},
+    args: {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Kittyply_edit1.jpg/220px-Kittyply_edit1.jpg',
+    },
+};
+
+export const Loading: Story = {
+    args: {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Kittyply_edit1.jpg/220px-Kittyply_edit1.jpg',
+        fallback: <Loader />,
+    },
+};
+
+export const ErrorFallback: Story = {
+    args: {
+        src: '',
+        errorFallback: <Text theme={TextTheme.ERROR} text="Error loading" />,
+    },
 };
