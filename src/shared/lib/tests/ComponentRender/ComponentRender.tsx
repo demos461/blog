0bb6039ae0fc3ae0807a178ee3c247412f79d6d5
demos461 <iamdemos461@gmail.com>
2023-11-29 16:30:53 +0300

    add tests for isolated components with cypress

--- src/shared/lib/tests/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender/ComponentRender.tsx
@@ -6,27 +6,53 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+// eslint-disable-next-line eliasxlii-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line eliasxlii-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface ComponentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+    theme?: Theme
 }
 
-export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+interface TestProviderProps {
+    children: ReactNode;
+    options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+    const {
+        children,
+        options = {},
+    } = props;
+
     const {
         route = '/',
         initialState,
         asyncReducers,
+        theme = Theme.DARK,
     } = options;
-    return render(
+
+    return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
-                    {component}
+                    <ThemeProvider initialTheme={theme}>
+                        <div className={`app ${theme}`}>
+                            {children}
+                        </div>
+                    </ThemeProvider>
                 </I18nextProvider>
             </StoreProvider>
-        </MemoryRouter>,
+        </MemoryRouter>
     );
+}
+
+export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+    return render(<TestProvider options={options}>{component}</TestProvider>);
 }
