5f525aabbc2f77905a9a122bbc7a4172ed8f10d3
demos461 <iamdemos461@gmail.com>
2023-12-01 14:29:56 +0300

    add arguments for selector

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -1,5 +1,6 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
+import { buildSelector } from '@/shared/lib/store/buildSelector';
 
 export const getArticlesPageIsLoading = (state: StateSchema) =>
     state.articlesPage?.isLoading;
@@ -20,3 +21,6 @@ export const getArticlesPageSearch = (state: StateSchema) =>
     state.articlesPage?.search ?? '';
 export const getArticlesPageType = (state: StateSchema) =>
     state.articlesPage?.type ?? ArticleType.ALL;
+export const [getArticleItemById] = buildSelector(
+    (state, id: string) => state.articlesPage?.entities[id],
+);
